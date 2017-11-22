61d52f9b6f845276b0693908123c9c4d318b688a
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2017-11-22 00:31:15 -0500

    doc

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mozilla-location-python
 Uses nmcli in a short, simple Mozilla Location Services with Wifi from Python. Extensible.
+
+## prereqs
+
+    pip install pandas requests
+
+## Usage
+
+    ./mozloc.py
+
+Returns `dict` containing `lat` `lng` `accuracy`.
+In urban areas, accuracy ~ 100 meters.
+
+## Notes
+[Inspired by](https://github.com/flyinva/mozlosh)
+
+[Alternative using Skyhook and geoclue](https://github.com/scivision/python-geoclue)
--- mozloc.py
+++ mozloc.py
@@ -3,6 +3,8 @@
 https://mozilla.github.io/ichnaea/api/geolocate.html
 you should get your own Mozilla Location Services API key
 
+Don't abuse the API or you'll get banned (excessive polling rate)
+
 Uses ``nmcli`` from Linux only. Could be extended to other tools and OS.
 """
 import subprocess
@@ -27,7 +29,13 @@ def get_nmcli():
     if req.status_code != 200:
         raise RuntimeError(ret.text)
 
-    return ret.json()
+    return req.json()
 
 if __name__ == '__main__':
+    """
+    output: lat lon [deg] accuracy [m]
+    """
     ret = get_nmcli()
+    loc = ret['location']
+
+    print(loc['lat'], loc['lng'], ret['accuracy'])
