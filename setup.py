228492f7a83caab04edd33712c6a1ba52a3470b5
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-05-08 00:53:41 -0400

    metadata

--- setup.py
+++ setup.py
@@ -17,5 +17,17 @@ setup(name='mozilla-location-python',
       extras_require={'tests':tests_require,
                       'kml':['simplekml']},
       python_requires='>=3.6',
+      classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: Console',
+      'Intended Audience :: Information Technology',
+      'Intended Audience :: System Administrators',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python :: 3.6',
+      'Programming Language :: Python :: 3.7',
+      'Topic :: System :: Networking',
+      'Topic :: Utilities',
+      ],
 	  )
 
