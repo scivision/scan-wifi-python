7177f33bfda333099b92956c6227fec645a2b518
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-18 21:57:59 -0500

    more robust test xfail

--- tests/test_all.py
+++ tests/test_all.py
@@ -2,21 +2,23 @@
 import pytest
 import mozloc
 import datetime
-import os
-
-CI = bool(os.environ['CI']) if 'CI' in os.environ else False
+import subprocess
 
 
-@pytest.mark.skipif(CI, reason="CI doesn't have WiFi")
 def test_nm_loc():
-    loc = mozloc.get_nmcli()
+    try:
+        loc = mozloc.get_nmcli()
+    except subprocess.CalledProcessError as e:
+        pytest.xfail(f'problem with NMCLI API--old NMCLI version?  {e}')
     assert isinstance(loc, dict)
     assert isinstance(loc['t'], datetime.datetime)
 
 
-@pytest.mark.skipif(CI, reason="CI doesn't have WiFi")
 def test_nm_connection():
-    mozloc.nm_config_check()
+    try:
+        mozloc.nm_config_check()
+    except subprocess.CalledProcessError as e:
+        pytest.xfail(f'problem with NMCLI WiFi API--do you have WiFi?  {e}')
 
 
 if __name__ == '__main__':
