e87f8112f4dbc69ed3d3d6dd4016465b202ee585
scivision <scivision@users.noreply.github.com>
2024-06-06 10:06:29 -0400

    macOS: catch removed Airport macOS 14.4+ issue

--- src/mozloc/cmd.py
+++ src/mozloc/cmd.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import os
 import functools
 import shutil
+import subprocess
 
 
 def running_as_root() -> bool:
@@ -17,10 +18,16 @@ def get_exe(name: str, path: str | None = None) -> str:
 
 
 def get_airport() -> str:
-    return get_exe(
+    exe = get_exe(
         "airport",
         "/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources",
     )
+
+    msg = subprocess.check_output(exe, text=True, timeout=10)
+    if "WARNING: The airport command line tool is deprecated" in msg:
+        raise EnvironmentError(msg)
+
+    return exe
 
 
 def get_nmcli() -> str:
